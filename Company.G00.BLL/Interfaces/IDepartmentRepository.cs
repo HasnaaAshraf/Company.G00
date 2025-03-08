0f341446ad63bd3cf570625c92a7c246bfaa10a2
HasnaaAshraf <hasnaaashraf2000@gmail.com>
2025-03-08 06:45:59 +0200

    Department Module BLL

---
+++ Company.G00.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G00.DAL.Models;
+
+namespace Company.G00.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department? Get(int id);
+
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+
+    }
+}
---
+++ Company.G00.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G00.BLL.Interfaces;
+using Company.G00.DAL.Data.Contexts;
+using Company.G00.DAL.Models;
+
+namespace Company.G00.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    // We Make DepartmentRepository For More Organized Code 
+    {
+
+        private readonly CompanyDbContext _context; // Null , ReadOnly For Only Make Only One Connection 
+
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext(); // Refer To This Instead Of Null 
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+
+    }
+}
