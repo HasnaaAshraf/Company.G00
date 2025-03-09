9b2e0fc2c7a0739aefa5088976616ba808b9dc95
HasnaaAshraf <hasnaaashraf2000@gmail.com>
2025-03-09 23:42:08 +0200

    Create (Create , Details ) Pages

--- Company.G00.PL/Controllers/DepartmentController.cs
+++ Company.G00.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G00.BLL.Interfaces;
 using Company.G00.BLL.Repositories;
+using Company.G00.DAL.Models;
+using Company.G00.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G00.PL.Controllers
@@ -20,5 +22,81 @@ namespace Company.G00.PL.Controllers
             var department = _departmentRepository.GetAll();
             return View(department); // Send Department As Model llView
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server Side Validation 
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+
+                var count = _departmentRepository.Add(department);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var departmentDetails = _departmentRepository.Get(id);
+
+            if(departmentDetails == null)
+            {
+                ViewBag.Message = "Department Details Not Found";
+                return View("NotFound");
+            }
+
+            var dto = new DetailsDepartmentDto
+            {
+                Code = departmentDetails.Code,
+                Name = departmentDetails.Name,
+                CreateAt = departmentDetails.CreateAt
+            };
+
+            return View(dto);
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var department = _departmentRepository.Get(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            var dto = new DetailsDepartmentDto
+            {
+                Code = department.Code,
+                Name = department.Name,
+                CreateAt = department.CreateAt
+            };
+
+            return View(dto);
+        }
+
+
     }
+
 }
---
+++ Company.G00.PL/Dtos/DetailsDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Company.G00.PL.Dtos
+{
+    public class DetailsDepartmentDto
+    {
+        [ReadOnly(true)]
+        public string Code { get; set; }
+
+        [ReadOnly(true)]
+        public string Name { get; set; }
+        
+        [ReadOnly(true)]
+        public DateTime CreateAt { get; set; }
+    }
+}
