e4f96b9c36766fb05acd4a1de69e04ade8928773
HasnaaAshraf <hasnaaashraf2000@gmail.com>
2025-03-12 16:49:10 +0200

    Implementation Of BLL For Employee Service Before Generic

---
+++ Company.G00.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G00.DAL.Models;
+
+namespace Company.G00.BLL.Interfaces
+{
+    internal interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee? Get(int id);
+
+        int Add (Employee employee);
+        int Update (Employee employee);
+        int Delete (Employee employee);
+
+    }
+}
