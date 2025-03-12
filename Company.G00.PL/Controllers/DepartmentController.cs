e937f6a79d4d0d3e89d47ada09e9245725666653
HasnaaAshraf <hasnaaashraf2000@gmail.com>
2025-03-12 04:23:18 +0200

    Increase Another Way To Details

--- Company.G00.PL/Controllers/DepartmentController.cs
+++ Company.G00.PL/Controllers/DepartmentController.cs
@@ -3,6 +3,7 @@ using Company.G00.BLL.Repositories;
 using Company.G00.DAL.Models;
 using Company.G00.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
 
 namespace Company.G00.PL.Controllers
 {
@@ -62,7 +63,7 @@ namespace Company.G00.PL.Controllers
             if(departmentDetails == null)
             {
                 ViewBag.Message = "Department Details Not Found";
-                return View("NotFound");
+                return View("Not Found Data");
             }
 
             var dto = new DetailsDepartmentDto
@@ -76,6 +77,26 @@ namespace Company.G00.PL.Controllers
 
         }
 
+        // Another Solution For Details
+
+        //[HttpGet]
+        //public IActionResult Details(int id)
+        //{
+        //    if (id == null) BadRequest("Invalid Id"); // 400
+
+        //    var departmentDetails = _departmentRepository.Get(id);
+
+        //    if (departmentDetails == null)
+        //    {
+        //        return NotFound(new { StatusCode = 404, message = "Not Found Data" }); // Built In Not Found Method 
+        //    }
+
+        //    return View(departmentDetails);
+
+        //}
+
+
+
         [HttpGet]
         public IActionResult Edit(int id)
         {
@@ -83,18 +104,44 @@ namespace Company.G00.PL.Controllers
 
             if (department == null)
             {
-                return NotFound();
+                return NotFound("Not Found Data");
             }
 
-            var dto = new DetailsDepartmentDto
-            {
+             var dto = new UpdateDepartmentDto()
+             {
                 Code = department.Code,
                 Name = department.Name,
                 CreateAt = department.CreateAt
-            };
+             };
 
-            return View(dto);
+
+               return View(dto);
         }
+
+        [HttpPost]
+        public IActionResult Edit(UpdateDepartmentDto model)
+        {
+            if (ModelState.IsValid) // Server Side Validation 
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+
+                var count = _departmentRepository.Update(department);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View(model);
+        }
+
 
 
     }
